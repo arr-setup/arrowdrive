2d3a45492faae7c32f5a36dca443b992f635d54b
happex <110610727+okayhappex@users.noreply.github.com>
2024-03-02 14:22:39 +0100

    Edit the converter
    * A class is not needed

--- adrv/utils.py
+++ adrv/utils.py
@@ -8,24 +8,22 @@ class Size:
             sz_unit += 1
 
         return f"{round(self.raw / (1000 ** sz_unit))}{'T' if sz_unit == 4 else 'G' if sz_unit == 3 else 'M' if sz_unit == 2 else 'k' if sz_unit == 1 else ''}B"
-    
-class Converter:
-    def __init__(self) -> None:
-        self.bytes = 1000 ** 0
-        self.kilo = 1000 ** 1
-        self.mega = 1000 ** 2
-        self.giga = 1000 ** 3
-        self.tera = 1000 ** 4
 
-    def convert(self, val: float, _from: str, _to: str) -> float:
-        _from = _from.upper()
-        _to = _to.upper()
+units = 1000 ** 0
+kilo = 1000 ** 1
+mega = 1000 ** 2
+giga = 1000 ** 3
+tera = 1000 ** 4
 
-        if _from not in ["B", "KB", "MB", "GB", "TB"] or _to not in ["B", "KB", "MB", "GB", "TB"]:
-            raise ValueError("Units must be B, kB, MB, GB or TB")
+def convert(val: float, _from: str, _to: str) -> float:
+    _from = _from.upper()
+    _to = _to.upper()
+
+    if _from not in ["B", "KB", "MB", "GB", "TB"] or _to not in ["B", "KB", "MB", "GB", "TB"]:
+        raise ValueError("Units must be B, kB, MB, GB or TB")
         
-        _from = self.bytes if _from == "B" else self.kilo if _from == "KB" else self.mega if _from == "MB" else self.giga if _from == "GB" else self.tera
-        _to = self.bytes if _to == "B" else self.kilo if _to == "KB" else self.mega if _to == "MB" else self.giga if _to == "GB" else self.tera
+    _from = units if _from == "B" else kilo if _from == "KB" else mega if _from == "MB" else giga if _from == "GB" else tera
+    _to = units if _to == "B" else kilo if _to == "KB" else mega if _to == "MB" else giga if _to == "GB" else tera
 
-        _diff = _from / _to
-        return val * _diff
+    _diff = _from / _to
+    return val * _diff
